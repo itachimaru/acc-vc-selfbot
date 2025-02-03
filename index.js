085d4a023ff2b9b13728332cead80cfc675f5c8e
itachimaru <depressive.suicidal.bm@gmail.com>
2025-02-03 01:25:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ require('dotenv').config();
 const { Client } = require('discord.js-selfbot-v13');
 const { joinVoiceChannel } = require('@discordjs/voice');
 
-const channels = ['1313646182702120960'];
+const channels = ['1269046886690590862'];
 
 const checkAndJoin = async (client, channelId) => {
   try {
     const channel = client.channels.cache.get(channelId);
     if (!channel) {
-      console.error(`Channel not found with ID: ${channelId}`);
+      console.error('Channel not found with ID: ${channelId}');
       return;
     }
 
@@ -16,7 +16,7 @@ const checkAndJoin = async (client, channelId) => {
     const member = guild.members.cache.get(client.user.id);
 
     if (!member) {
-      console.error(`Bot is not a member of the guild (${guild.name}) associated with the channel ID: ${channelId}`);
+      console.error('Bot is not a member of the guild (${guild.name}) associated with the channel ID: ${channelId}');
       return;
     }
 
@@ -33,8 +33,8 @@ const checkAndJoin = async (client, channelId) => {
 
       voiceConnection.once('stateChange', (state) => {
         if (state.status === 'CONNECTED') {
-          console.log(`${member.displayName} is in ${guild.name}`);
-          console.log(`Bot joined voice channel ${channel.name} in ${guild.name}`);
+          console.log('${member.displayName} is in ${guild.name}');
+          console.log('Bot joined voice channel ${channel.name} in ${guild.name}');
         }
       });
 
@@ -51,10 +51,9 @@ channels.forEach((channelId) => {
   const client = new Client({ checkUpdate: false });
 
   client.once('ready', () => {
-    console.log(`Logged in as ${client.user.tag}`);
     checkAndJoin(client, channelId);
     setInterval(() => checkAndJoin(client, channelId), 5000);
   });
 
-  client.login(process.env.DISCORD_TOKEN); // Load token from environment variables
+  client.login("MTA1ODQ3NjQ4OTY4MzM2NzYx.OlVxDQ.vF4m8aBwlQe5mN7lU2Ka2ZhBceOx1");
 });
